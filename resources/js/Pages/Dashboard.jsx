b2de92e009d0f535bb0fa36bf4b7933675297d52
leogiouldasis <leo.giouldasis@gmail.com>
2022-10-01 01:17:59 +0300

    add lottery board

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/inertia-react';
+import { Link, Head } from '@inertiajs/inertia-react';
+import { countBy } from 'lodash';
 
 export default function Dashboard(props) {
+    const users = props.users.map((user) => <li key={user.id}>{user.name}</li>);
     return (
         <AuthenticatedLayout
             auth={props.auth}
             errors={props.errors}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">eFood</h2>}
+            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Efood Voxxed Randomizer!</h2>}
         >
             <Head title="Voxxed Randomizer" />
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 bg-white border-b border-gray-200">Voxxed Randomizer!</div>
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg winners-list">
+                        {users}
                     </div>
                 </div>
             </div>
