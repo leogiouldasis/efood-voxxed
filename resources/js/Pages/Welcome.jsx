2c6a617ec00efd500d28078fdb00c8a7f80cf237
leogiouldasis <leo.giouldasis@gmail.com>
2022-10-01 03:13:50 +0300

    fix efood url

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -153,9 +153,9 @@ export default function Welcome(props) {
                             <div className="p-6 border-t border-gray-200 dark:border-gray-700 md:border-l">
                                 {props.auth.user ? (
                                     <>
-                                        <Link href={'https://www.e-food.gr/'} target="_blank" className="text-m text-gray-700 dark:text-gray-500 underline">
+                                        <a href={'https://www.e-food.gr/'} target="_blank" className="text-m text-gray-700 dark:text-gray-500 underline">
                                             Empty Stomach?
-                                        </Link>
+                                        </a>
                                     </>
                                 ) : (
                                     <> 
